73a9c3df3065f30a7ed424a55be662c198776edd
Marcos J Estrada <mje72@login6.(none)>
2015-08-11 11:57:29 -0500

    Added Leaft Count to source Files

--- Source_2_leaf/Main.cpp
+++ Source_2_leaf/Main.cpp
@@ -1,4 +1,5 @@
-//Original Program
+//Original Code plus degree cover 1 Optimization when largest degree is one
+
 
 #include "Tree.h"
 #include <list>
@@ -9,13 +10,15 @@
 #include <algorithm>
 #include <utility>
 #include <string>
+#include <limits>
 
 using namespace std;
 
 vector<int> DFSCover(list<list<int> > graph, int edge_count);
 bool inVector(vector<int> &v,  int &n);
+bool inVector(vector<pair<int,int> > &v, pair<int,int> &p);
 bool checkVertices(list<list<int> >&graph, vector<int> &cover_Vertex, int edge_count);
-int leaf_count = 0;//global so not to re-define function
+int leaf_count = 0;
 
 int main(int argc, char* argv[]){
 
@@ -44,8 +47,6 @@ int main(int argc, char* argv[]){
 		int vertical_number = 1;
 		int horizontal_number = 1;
 		edge_count = 0;//discard the number that was read at top of file
-
-		//while(!ifs.eof()){
 		while(ifs){
 			ifs>>ws;
 			getline(ifs,line);
@@ -169,8 +170,9 @@ int main(int argc, char* argv[]){
 			cout<<(*it1)<< " ";
 		}
 		
-	}
-	cout<<endl;*/
+	}*/
+	cout<<"Done reading in graph";
+	cout<<endl;
 	vector<int> answer = DFSCover(graph, edge_count);
 
 	cout<<"vector cover answer"<<endl;
@@ -179,14 +181,15 @@ int main(int argc, char* argv[]){
 	}
 
 	cout<<"cover size: "<<answer.size()<<endl;
-	cout<<"leaf count: "<<leaf_count<<endl;
-	
+	cout<<"Leaf Count: "<<leaf_count<<endl;
+
 	return 0;
 }
 
 
 vector<int> DFSCover(list<list<int> > graph,int edge_count){
-	vector<int> curr_min_cover(1000);//initiliaze for initial comparison //maybe fill it to max_int value?
+	int max = std::numeric_limits<int>::max();
+	vector<int> curr_min_cover(max);//initiliaze for initial comparison //maybe fill it to max_int value?
 	
 	stack<TreeNode*> node_stack;
 	
@@ -201,8 +204,8 @@ vector<int> DFSCover(list<list<int> > graph,int edge_count){
 		node_stack.pop();
 
 		//check if node has cover, compare to new cover to find min cover
-		if(checkVertices(graph, tmp->cover_Vertex, edge_count) && tmp->cover_Vertex.size()<= curr_min_cover.size())
-			curr_min_cover = tmp->cover_Vertex; //return tmp->cover_Vertex;
+		if(tmp->current_covered_edges.size() == edge_count)
+			curr_min_cover = tmp->cover_Vertex; 
 
 
 
@@ -216,8 +219,8 @@ vector<int> DFSCover(list<list<int> > graph,int edge_count){
 			list<list<int> >::iterator it = tmp_graph.begin();
 
 				
-			int count = 0;
-			int largest_count=0;
+			int count = 0; //number of edges
+			int largest_count=0; // larget number of edges
 
 			for(it; it!=tmp_graph.end(); it++){
 
@@ -237,68 +240,96 @@ vector<int> DFSCover(list<list<int> > graph,int edge_count){
 			}
 
 			
-			
-			TreeNode* right = new TreeNode;
-			TreeNode* left = new TreeNode;
+			if(largest_count > 1){ 
+				TreeNode* right = new TreeNode;
+				TreeNode* left = new TreeNode;
 
-			right->cover_Vertex = tmp->cover_Vertex;
-			left->cover_Vertex = tmp->cover_Vertex;
+				right->cover_Vertex = tmp->cover_Vertex;
+				left->cover_Vertex = tmp->cover_Vertex;
 
-			list<int>::iterator vertex_it = (*current_largest).begin();
+				list<int>::iterator vertex_it = (*current_largest).begin();
 
-			//vertex is in
-			if(!inVector(left->cover_Vertex, (*vertex_it)))
-				left->cover_Vertex.push_back((*vertex_it));
+				//vertex is in
+				if(!inVector(left->cover_Vertex, (*vertex_it)))
+					left->cover_Vertex.push_back((*vertex_it));
 
-
-			//neighbors are in	
-			vertex_it++;
-			for(vertex_it; vertex_it!=(*current_largest).end(); vertex_it++){
-				if(!inVector(right->cover_Vertex, (*vertex_it)))
-					right->cover_Vertex.push_back((*vertex_it));
-			}
-
-			//delete V
-
-			int deleted_vertex = (*(*current_largest).begin());
-
-			tmp_graph.erase(current_largest);
-			//delete V from adjacency list
-
-			list<list<int> >::iterator it1=tmp_graph.begin(); 
-			list<int>::iterator it2;
-
-			for(it1; it1 != tmp_graph.end(); it1++){
-				for(it2 = (*it1).begin(); it2 != (*it1).end(); it2++){
-
-					if((*it2) == deleted_vertex){
-						list<int>::iterator it3 = it2;
-						(*it1).erase(it3); 
-						it2--;
-					}
-					
-
+				//neighbors are in	
+				vertex_it++;
+				for(vertex_it; vertex_it!=(*current_largest).end(); vertex_it++){
+					if(!inVector(right->cover_Vertex, (*vertex_it)))
+						right->cover_Vertex.push_back((*vertex_it));
 				}
-			}
+
+				//insert cover edges to current covered edges
+				list<int>::iterator horizontal_iterator = (*current_largest).begin();
+				int first = (*horizontal_iterator);
+				++horizontal_iterator;
+				for(horizontal_iterator; horizontal_iterator != (*current_largest).end(); ++horizontal_iterator){
+
+					int second = (*horizontal_iterator);
+					int a,b;
+					if(second < first){
+						a = second;
+						b = first;
+					}
+					else{
+						a = first;
+						b = second;
+					}
+
+					pair<int,int> pair1(a,b);
+					if(!inVector(right->current_covered_edges,pair1))
+						right->current_covered_edges.push_back(pair1);
+					if(!inVector(left->current_covered_edges,pair1))
+						left->current_covered_edges.push_back(pair1);
+				}
+				
+				//Make left child graph, delete V
+				int deleted_vertex = (*(*current_largest).begin());
+				tmp_graph.erase(current_largest);
 
 
-			right->graph = tmp_graph;
-			left->graph = tmp_graph;
+				
+				//delete V from adjacency list for left child graph
+				list<list<int> >::iterator it1=tmp_graph.begin(); 
+				list<int>::iterator it2;
 
-			tmp->right_child = right;
-			tmp->left_child = left;
+				for(it1; it1 != tmp_graph.end(); it1++){
+					for(it2 = (*it1).begin(); it2 != (*it1).end(); it2++){
+						if((*it2) == deleted_vertex){
+							list<int>::iterator it3 = it2;
+							(*it1).erase(it3); 
+							it2--;
+						}
+					}
+				}
+				right->graph = tmp_graph;
+				left->graph = tmp_graph;
 
-			node_stack.push(right);
-			node_stack.push(left);
+				tmp->right_child = right;
+				tmp->left_child = left;
+
+				node_stack.push(right);
+				node_stack.push(left);
+
+			} else if(largest_count == 1){ //largest degree is one so just complete the vector cover here don't branch out
+				for(list<list<int> >::iterator it = tmp_graph.begin(); it != tmp_graph.end(); it++){
+					list<int>::iterator it_v = (*it).begin();
+						if( (++it_v) != (*it).end()){ // if vertex does have edge
+							if(!inVector(tmp->cover_Vertex, (*it_v)))
+								tmp->cover_Vertex.push_back((*it_v));
+						}
+				}
+
+				if(tmp->cover_Vertex.size() < curr_min_cover.size())
+					curr_min_cover = tmp->cover_Vertex;
+			} //else
 		} else{
-			++leaf_count;//be careful, this variable is global
+			++leaf_count;
 		}
 
 		delete tmp;//free memory 
 	}
-
-	//return empty vector
-	//vector<int> empty;
 	return curr_min_cover;
 }
 
@@ -318,46 +349,3 @@ bool inVector(vector<pair<int,int> > &v, pair<int,int> &p){//overloaded
 	return false;
 }
 
-bool checkVertices(list<list<int> >&graph, vector<int> &cover_Vertex, int edge_count){
-	vector<pair<int,int> > covered_Edges;
-
-	for(list<list<int> >::iterator it = graph.begin(); it != graph.end(); it++){
-
-		list<int>::iterator it1 = (*it).begin();
-		int first = (*it1);
-
-		if(inVector(cover_Vertex,(*it1))){
-			it1++;
-			for(it1; it1 != (*it).end(); it1++){
-
-				int second = (*it1);
-				int a,b;
-				if(second < first){
-					a = second;
-					b = first;
-				}
-				else{
-					a = first;
-					b = second;
-				}
-
-				pair<int,int> pair1(a,b);
-				if(!inVector(covered_Edges,pair1))
-					covered_Edges.push_back(pair1);
-			}
-		}
-	}
-
-	/*cout<<"cover"<<endl;
-	for(int i=0; i<cover_Vertex.size(); i++){
-		cout<<cover_Vertex[i]<<" ";
-	}
-	cout<<endl;
-	cout<<"Covered Edges"<<endl;
-	for(int i = 0; i<covered_Edges.size(); ++i){
-		cout<<covered_Edges[i].first<<" "<<covered_Edges[i].second<<endl;
-	}*/
-
-	return covered_Edges.size() == edge_count;
-
-}
