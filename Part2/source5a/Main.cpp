33551b35b320ed542459fb38cf3c9de9279a98c3
Marcos J Estrada <mje72@login4.(none)>
2015-10-15 12:01:59 -0500

    Added both versions of source5

---
+++ Part2/source5a/Main.cpp
@@ -0,0 +1,35 @@
+nclude "TreeSearch.h"
+#include <list>
+#include <stack>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <string>
+
+using namespace std;
+int main(int argc, char* argv[]){
+
+	list<list<int> > graph;
+	int edge_count = 0;
+	int leaf_count = 0;
+	
+	read_graph(graph, edge_count, argv);
+	
+	vector<int> answer = DFSCover(graph, edge_count, leaf_count);
+
+	if(isCover(graph, answer, edge_count)){
+		cout<<"vector cover answer"<<endl;
+		for(int i = 0; i<answer.size(); ++i){
+			cout<< answer[i]<< " ";
+		}
+
+		cout<<"cover size: "<<answer.size()<<endl;
+		cout<<"leaf count: "<<leaf_count<<endl;
+	} else {
+		cout<<"Cover not found"<<endl;
+	}
+	 
+	return 0;
+}
---
+++ Part2/source5a/Readme.md
@@ -0,0 +1,1 @@
+Original Version 5. Added while loop to degree one optimization from version 4. Still contains extra copy in dfs cover funtion. 
