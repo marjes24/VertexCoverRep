71961c3ea58efbe4a503c47cc6d37e08d38844c1
Marcos J Estrada <mje72@login3.(none)>
2015-12-01 12:21:33 -0600

    Updating files

--- Part3/source4/source/Main.cpp
+++ Part3/source4/source/Main.cpp
@@ -20,10 +20,10 @@ int main(int argc, char* argv[]){
 	read_graph(graph, edge_count, argv);
 	vector<int> answer = DFSCover(graph, edge_count, leaf_count);
 	if(isCover(graph, answer, edge_count)){
-		cout<<"vector cover answer"<<endl;
+		/*cout<<"vector cover answer"<<endl;
 		for(int i = 0; i<answer.size(); ++i){
 			cout<< answer[i]<< " ";
-		}
+		}*/
 		cout<<"cover size: "<<answer.size()<<endl;
 		cout<<"leaf count: "<<leaf_count<<endl;
 	} else {
