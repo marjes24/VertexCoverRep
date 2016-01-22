45d1c6baa535d93f624bb47754acc25ef46befae
Marcos J Estrada <mje72@login7.(none)>
2016-01-21 22:09:15 -0600

    updating

---
+++ backedupTestFiles/readOutpu.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+int main(int argc, char* argv[]){
+
+	ifstream ifs;
+	ifs.open(argv[1]);
+	stringstream ss;
+	string line;
+	string first_word;
+	string second_word;
+	while(ifs){
+		ifs>>ws;
+		getline(ifs,line);
+		ss<<line;
+		while(ss){
+			ss>>first_word;
+			if(first_word == "real" || first_word == "cover" || first_word == "leaf")
+				cout<<line<<endl;
+		}
+		ss.clear();
+		//cout<<line<<endl;;
+	}
+
+	ifs.close();
+	return 0;
+}
